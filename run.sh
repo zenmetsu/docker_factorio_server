616d78c45f612865ee42f810a67c967779a03da8
David Andersen <git@davidandersen.us>
2016-10-26 17:27:28 +0000

    mount volumes to /tmp

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
+mkdir -p /tmp/factorio
+mkdir -p /tmp/factorio/mods
+mkdir -p /tmp/factorio/saves
+cp -n server-settings.example.json /tmp/factorio/server-settings.json
+cp -n map-gen-settings.example.json /tmp/factorio/map-gen-settings.json
 sudo docker run --rm -it \
-	-v /home/vagrant/docker_factorio_server/server-settings.json:/opt/factorio/data/server-settings.json \
-	-v /home/vagrant/docker_factorio_server/map-gen-settings.json:/opt/factorio/data/map-gen-settings.json \
-	-v /data/mods:/opt/factorio/mods \
-	-v /data/saves:/opt/factorio/saves \
+	-v /tmp/factorio/server-settings.json:/opt/factorio/data/server-settings.json \
+	-v /tmp/factorio/map-gen-settings.json:/opt/factorio/data/map-gen-settings.json \
+	-v /tmp/factorio/mods:/opt/factorio/mods \
+	-v /tmp/factorio/saves:/opt/factorio/saves \
 	--name factorio \
-	factorio
+	factorio "$@"
