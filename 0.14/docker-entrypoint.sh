9fbd7d92bcf081c2b8f7b9968bb67a033cfd50cb
David Andersen <git@davidandersen.us>
2016-11-01 20:31:24 +0000

    use sh instead of bash

--- 0.14/docker-entrypoint.sh
+++ 0.14/docker-entrypoint.sh
@@ -1,34 +1,35 @@
-#!/bin/bash -x
+#!/bin/sh -x
 
 set -e
 
-SAVES=/opt/factorio/saves
+SAVES=/factorio/saves
+CONFIG=/factorio/config
 
-mkdir -p /factorio/saves
+mkdir -p $SAVES
 mkdir -p /factorio/mods
-mkdir -p /factorio/config
+mkdir -p $CONFIG
 
-if [ ! -f /factorio/config/rconpw ]; then
-  echo $(pwgen 15 1) > /factorio/config/rconpw
+if [ ! -f $CONFIG/rconpw ]; then
+  echo $(pwgen 15 1) > $CONFIG/rconpw
 fi
 
-if [ ! -f /factorio/config/server-settings.json ]; then
-  cp /opt/factorio/data/server-settings.example.json /factorio/config/server-settings.json
+if [ ! -f $CONFIG/server-settings.json ]; then
+  cp /opt/factorio/data/server-settings.example.json $CONFIG/server-settings.json
 fi
 
-if [ ! -f /factorio/config/map-gen-settings.json ]; then
-  cp /opt/factorio/data/map-gen-settings.example.json /factorio/config/map-gen-settings.json
+if [ ! -f $CONFIG/map-gen-settings.json ]; then
+  cp /opt/factorio/data/map-gen-settings.example.json $CONFIG/map-gen-settings.json
 fi
 
 if ! find -L $SAVES -iname \*.zip -mindepth 1 -print | grep -q .; then
   /opt/factorio/bin/x64/factorio \
     --create $SAVES/_autosave1.zip  \
-    --map-gen-settings /opt/factorio/data/map-gen-settings.json
+    --map-gen-settings $CONFIG/map-gen-settings.json
 fi
 
 exec /opt/factorio/bin/x64/factorio \
   --port 34197 \
   --start-server-load-latest \
-  --server-settings /opt/factorio/data/server-settings.json \
+  --server-settings $CONFIG/server-settings.json \
   --rcon-port 27015 \
-  --rcon-password "$(cat /factorio/config/rconpw)"
+  --rcon-password "$(cat $CONFIG/rconpw)"
